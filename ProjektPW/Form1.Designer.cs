cab7ec351be3a7fe2c0b41499877eedb8910d3be
Wojciech Zalewski <zalewskiwoj8@gmail.com>
2021-06-05 17:17:12 +0200

    added timer

--- ProjektPW/Form1.Designer.cs
+++ ProjektPW/Form1.Designer.cs
@@ -30,14 +30,22 @@ namespace ProjektPW
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.trackBar1 = new System.Windows.Forms.TrackBar();
             this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // timer1
             // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 550;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // trackBar1
@@ -45,7 +53,7 @@ namespace ProjektPW
             this.trackBar1.BackColor = System.Drawing.SystemColors.Control;
             this.trackBar1.Cursor = System.Windows.Forms.Cursors.Default;
             this.trackBar1.LargeChange = 100;
-            this.trackBar1.Location = new System.Drawing.Point(973, 605);
+            this.trackBar1.Location = new System.Drawing.Point(964, 605);
             this.trackBar1.Maximum = 1000;
             this.trackBar1.Minimum = 100;
             this.trackBar1.Name = "trackBar1";
@@ -60,11 +68,52 @@ namespace ProjektPW
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(895, 619);
+            this.label1.Location = new System.Drawing.Point(5, 43);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(62, 20);
             this.label1.TabIndex = 1;
             this.label1.Text = "550[ms]";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(877, 575);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(373, 92);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Timer";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoCheck = false;
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Enabled = false;
+            this.radioButton1.Location = new System.Drawing.Point(48, 3);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(17, 16);
+            this.radioButton1.TabIndex = 3;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1056, 540);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 29);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Pause";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1156, 540);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(94, 29);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Restart";
+            this.button2.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
@@ -72,11 +121,18 @@ namespace ProjektPW
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
             this.ClientSize = new System.Drawing.Size(1262, 673);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "ProjektPW Zalewski IY4";
             ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -87,6 +143,10 @@ namespace ProjektPW
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.TrackBar trackBar1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- ProjektPW/Form1.cs
+++ ProjektPW/Form1.cs
@@ -12,19 +12,26 @@ namespace ProjektPW
 {
     public partial class Form1 : Form
     {
+        private bool timer1_status = false;
         public Form1()
         {
             InitializeComponent();
         }
-
+        
         private void timer1_Tick(object sender, EventArgs e)
         {
-
+            if (timer1_status)
+                radioButton1.Checked = true;
+            else
+                radioButton1.Checked = false;
+            timer1_status = !timer1_status;
+           
         }
 
         private void trackBar1_Scroll(object sender, EventArgs e)
         {
             label1.Text = Convert.ToString(trackBar1.Value)+"[ms]";
+            timer1.Interval = trackBar1.Value;
         }
     }
 }
